678ce47395a870fa942ff86937f55e5215c7d2bf
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-03-09 22:28:41 -0700

    Why shop with us text

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -8,11 +8,21 @@ export const WhyUs = () => {
         <img src={whyUs} alt="" className=""/>
       </div>
         <div>
+          <h2>WHY SHOP WITH US?</h2>
         </div>
         <div>
-            <p></p>
-            <p></p>
-            <p></p>
+          <div>
+            <h3>100% ORGANIC</h3>
+            <p>Our plants come from our greenhouse and local organic farms.</p>
+          </div>
+          <div>
+            <h3>30-DAY GUARANTEE</h3>
+            <p>If your plants arrived damaged, we'll replace it for free!</p>
+          </div>
+          <div>
+            <h3>PLANT CONSULTATION</h3>
+            <p>If you're struggling to care for your new plant, you can book an appointment with a plant-care specialist at a low cost or check our blog!</p>
+          </div>
         </div>
     </div>
   )
