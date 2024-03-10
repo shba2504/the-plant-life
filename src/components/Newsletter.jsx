99336ab118da69bc0fb755c3dfa466d221886ac6
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-03-09 22:23:17 -0700

    footer text

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -5,12 +5,42 @@ export const Newsletter = () => {
     <div className="flex mt-4">
       <div>
         <h3 className="tracking-widest">GET IN THE KNOW</h3>
-        <p>blah blah</p>
+        <p></p>
         <input type="email" name="email" id="email" placeholder="Enter your email"></input>
         <button className="bg-green text-white" type="submit" aria-label="">Subscribe</button>
       </div>
       <div>
-        <p></p>
+        <div>
+          <h3>ORDERS</h3>
+          <p>Track My Order</p>
+          <p>Shipping and Handling</p>
+          <p>30-Day Guarantee</p>
+          <p>FAQs</p>
+        </div>
+        <div>
+          <h3>CONTACT US</h3>
+          <p>Phone</p>
+          <p>Email</p>
+          <p>Live Chat</p>
+          <p>Report Issue</p>
+          <p>Business Inquiries</p>
+        </div>
+        <div>
+          <h3>THE PLANT LIFE</h3>
+          <p>Blog</p>
+          <p>Plant Care</p>
+          <p>Gardening</p>
+          <p>Consultations</p>
+          <p>Locations</p>
+        </div>
+        <div>
+          <h3>POLICIES</h3>
+          <p>Returns</p>
+          <p>Terms of Use</p>
+          <p>Privacy Policy</p>
+          <p>Become an Affiliate</p>
+          <p>Do Not Sell My Info</p>
+        </div>
       </div>
     </div>
   )
