0b793e33706e399586601d31ba0b05a3c744003e
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-03-17 22:26:47 -0600

    planters component updates

--- src/components/Planters.jsx
+++ src/components/Planters.jsx
@@ -3,7 +3,7 @@ import planters1 from "../assets/images/planters1.jpg"
 import planters2 from "../assets/images/planters2.jpg"
 import planters3 from "../assets/images/planters3.jpg"
 import planters4 from "../assets/images/planters4.jpg"
-import plantersCollection from "../assets/images/plantersCollection.jpg"
+import planters5 from "../assets/images/planters5.jpg"
 import { MdOutlineArrowRightAlt } from "react-icons/md";
 
 export const Planters = () => {
@@ -20,19 +20,19 @@ export const Planters = () => {
       </div>
       <div className="flex">
         <div>
-          <img src={planters1} alt="plants in gold containers on a wooden table" />
+          <img src={planters1} alt="plants in gold containers on a wooden table" className="aspect-[4/3]" />
         </div>
         <div>
-          <img src={planters3} alt="three beige planters with two on a pedestal" />
+          <img src={planters3} alt="three beige planters with two on a pedestal" className="aspect-[4/3]" />
         </div>
         <div>
-          <img src={planters4} alt="3 bronze planters" />
+          <img src={planters2} alt="3 copper planters" className="aspect-[4/3]" />
         </div>
         <div>
-          <img src={planters2} alt="2 plants, one dark and one light" />
+          <img src={planters5} alt="2 plants, one dark and one light" className="aspect-[4/3] object-cover "/>
         </div>
         <div>
-          <img src={plantersCollection} alt="a collection of various planters: white, brown, and gold" />
+          <img src={planters4} alt="a collection of various planters: white, brown, and gold" className="aspect-[4/3] object-cover"/>
         </div>
       </div>
     </div>
