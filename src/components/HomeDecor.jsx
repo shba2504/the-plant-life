b6b168b6187160e1ce3611ae61a4d10c8c93767c
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-03-17 22:48:24 -0600

    switched order homeDecor

--- src/components/HomeDecor.jsx
+++ src/components/HomeDecor.jsx
@@ -6,7 +6,7 @@ export const HomeDecor = () => {
   return (
     <div className="py-4">
       <div className="flex items-center flex-col md:flex-row mt-4 bg-gray">
-        <div className="my-2 px-10">
+        <div className="my-2 px-10 order-last md:order-first">
           <h2 className="text-5xl lg:text-6xl text-left font-light">Plants For Every Space.</h2>
           <button className="bg-green mt-6 lg:text-lg focus:bg-white text-white focus:text-green font-semi-bold px-12 md:px-12 lg:px-12 p-2 md:p-1 lg:p-2 rounded-md">Find Inspiration</button>
         </div>
