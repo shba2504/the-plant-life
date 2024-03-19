3b8216b1226e9ee3b2d972fbef4d51a52af50ba8
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-03-18 19:10:41 -0600

    fixed homeDecor

--- src/components/HomeDecor.jsx
+++ src/components/HomeDecor.jsx
@@ -4,11 +4,11 @@ import homeDecor2 from "../assets/images/homeDecor2.jpg"
 
 export const HomeDecor = () => {
   return (
-    <div className="py-4">
+    <div className="pt-4">
       <div className="flex items-center flex-col md:flex-row mt-4 bg-gray">
-        <div className="my-2 px-10 order-last md:order-first">
-          <h2 className="text-5xl lg:text-6xl text-left font-light">Plants For Every Space.</h2>
-          <button className="bg-green mt-6 lg:text-lg focus:bg-white text-white focus:text-green font-semi-bold px-12 md:px-12 lg:px-12 p-2 md:p-1 lg:p-2 rounded-md">Find Inspiration</button>
+        <div className="lg:py-4 px-10 order-last md:order-first">
+          <h2 className="text-5xl lg:text-6xl text-left font-light mr-32 md:mr-10 py-6 md:py-0 ">Plants For Every Space.</h2>
+          <button className="bg-green mt-8 lg:text-lg focus:bg-white text-white focus:text-green font-semi-bold px-12 md:px-8 lg:px-12 p-2 md:p-1 rounded-md">Get Inspired</button>
         </div>
         <div>
           <img src={homeDecor2} alt="" className="aspect-[3/3] object-cover"/>
