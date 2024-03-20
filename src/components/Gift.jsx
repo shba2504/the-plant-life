358042897acefa9092020cb3cdf980c9baff3cd8
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-03-19 22:49:48 -0600

    work on gift and homeDecor components

--- src/components/Gift.jsx
+++ src/components/Gift.jsx
@@ -8,10 +8,10 @@ export const Gift = () => {
         <div>
           <img src={gift} alt="bouquet of flowers on a wooden table" className="aspect-[4/3] object-cover" />
         </div>
-        <div className="grid items-center py-2">
+        <div className="grid items-center py-2 lg:py-0">
           <h2 className="text-5xl lg:text-6xl text-center font-light">Give The Gift That Will Make Them <span className="text-green semi-bold">Smile.</span></h2>
-          <div className="flex justify-center">
-            <button type="submit" className="bg-green text-white lg:text-lg focus:bg-white focus:text-green font-semi-bold px-12 py-2 mt-8 md:p-1 rounded-md">Shop Bouquets</button>
+          <div className="flex justify-center py-2">
+            <button type="submit" className="bg-green text-white lg:text-lg focus:bg-white focus:text-green font-semi-bold px-12 lg:px-16 py-2 mt-8 rounded-md">Shop Bouquets</button>
           </div>
         </div>
       </div>
--- src/components/HomeDecor.jsx
+++ src/components/HomeDecor.jsx
@@ -7,7 +7,7 @@ export const HomeDecor = () => {
     <div className="py-8 px-10">
       <div className="grid md:grid-cols-2 bg-gray">
         <div className="grid py-4 px-10 order-last md:order-first ">
-          <h2 className="text-5xl lg:text-6xl text-center font-light">Plants For Every Space.</h2>
+          <h2 className="text-5xl lg:text-6xl text-center font-light"><span className="text-green">Plants</span> For Every Space.</h2>
           <button className="bg-green lg:text-lg focus:bg-white text-white focus:text-green font-semi-bold px-12 py-2 mt-8 md:p-1 rounded-md">Get Inspired</button>
         </div>
         <div>
